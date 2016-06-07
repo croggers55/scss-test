8c4e567ddaa5c4b75605d4ed8a9464fe1adadb47
Craig Farris <cfarris@ucdavis.edu>
2016-06-07 11:44:40 -0700

    first commit after running grunt workflow - woohoo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,26 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-   // Tasks
-  });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['']);
-
-};
-
-
-module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
-			dist: {
-				files: {
-					'style/style.css' : 'sass/style.scss'
-				}
+			dev: {
+				files: [{
+					expand: true,
+					cwd: 'source/css/',
+					src: ['*.scss'],
+					dest: 'local_resources/css',
+					ext: '.css'
+				}]
 			}
 		},
-		// sass: {
-		// 	dist: {
-		// 		files: [{
-		// 			expand: true,
-		// 			cwd: 'styles',
-		// 			src: ['*.scss'],
-		// 			dest: '../public',
-		// 			ext: '.css'
-		// 		}]
-		// 	}
-		// },
 		watch: {
 			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
+				files: '**/*.{scss,sass}',
+				tasks: ['sass:dev']
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default',['watch']);
+	grunt.registerTask('default',['sass:dev','watch']);
 }
